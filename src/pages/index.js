f527933d19bab8bb6cf27cd8ef6bfb04fdbb97c0
Jesus Herrera <jesusherrera1594@gmail.com>
2020-11-19 17:29:20 -0600

    initial commit of gatsby project

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+const IndexPage = () => (
+  <h1>Hola mundo desde gatsby</h1>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+const SecondPage = () => (
+  <h1>p2</h1>
+)
+
+export default SecondPage
